e4de9f7a3c3f61f5f06369128ead3f864b000ec4
fuzhengjia@gmail.com <19831226>
2014-03-21 18:53:08 +0800

    set nextCheckTime backto +60000

--- src/main/java/storm/resa/measure/TracedMeasurementCollector.java
+++ src/main/java/storm/resa/measure/TracedMeasurementCollector.java
@@ -43,7 +43,7 @@ public class TracedMeasurementCollector extends RedisMetricsCollector {
             // No tuple metrics found, add it to waiting list
             // check it after 1 min
             while (true) {
-                Long nextCheckTime = System.currentTimeMillis() + 20000;
+                Long nextCheckTime = System.currentTimeMillis() + 60000;
                 if (waitingTuples.containsKey(nextCheckTime)) {
                     // avoid time key conflict
                     try {
